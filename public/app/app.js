b2db43006c4cf6aa24dab7769829a9a10c3942a8
Zyggy Lau <zyggy.lau@schoolteam.com.hk>
2018-12-10 01:40:32 +0800

    minor layout update

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,5 @@
 //TODO: angular using RequireJS
+//TODO: feedbacks page pagination
 angular.module('userApp', ['appRoutes', 'userFeedbackServices', 'authServices', 'mainControllers', 'fileModelDirective'])
     .config(function ($httpProvider) {
         $httpProvider.interceptors.push('AuthInterceptors');
